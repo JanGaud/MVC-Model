f10e9f4f0d78bdf3638053465bb9997afdcd787e
JanGaud <JanisGaudreault.jg@gmail.com>
2022-09-13 14:49:49 -0400

    retouche css

--- web21622/mvc/views/base/welcome.php
+++ web21622/mvc/views/base/welcome.php
@@ -1,4 +1,22 @@
-<i>Welcome</i>
-<p>
-  MVC framework
-</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Travail Pratique</title>
+</head>
+<body>
+  <header>
+    <nav>
+      <div class="navImg">
+        <img src="web21622/mvc/resources/img/WebDevLogo.png" alt="">
+      </div>
+        <a href="#">Inscription</a>
+        <a href="#">Login</a>
+        <a href="#">Forum</a>
+    </nav>
+  </header>
+  
+</body>
+</html>
