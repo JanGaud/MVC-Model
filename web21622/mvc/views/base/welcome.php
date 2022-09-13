e7a4815718b2d3a041d395a696b7991674d05844
JanGaud <JanisGaudreault.jg@gmail.com>
2022-09-13 15:00:30 -0400

    e

--- web21622/mvc/views/base/welcome.php
+++ web21622/mvc/views/base/welcome.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Travail Pratique</title>
-</head>
-<body>
   <header>
     <nav>
       <div class="navImg">
@@ -17,6 +8,3 @@
         <a href="#">Forum</a>
     </nav>
   </header>
-  
-</body>
-</html>
