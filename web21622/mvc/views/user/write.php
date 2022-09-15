716d4ba2b4d014e1d69b53c7b27f6d97a8e91880
JanGaud <JanisGaudreault.jg@gmail.com>
2022-09-15 19:41:31 -0400

    Update write.php

--- web21622/mvc/views/user/write.php
+++ web21622/mvc/views/user/write.php
@@ -1,7 +1,7 @@
 
 
 <form class="redaction" action="post">
-    <div class="flexRowClose"><h3>Rédigez votre article</h3> <small>date/xxxx/xxxx</small></div>
-        <textarea name="article" id="article" cols="30" rows="10">...</textarea>
+    <div class="flexRowClose"><h3>Rédigez votre article</h3> <small><?=date("Y-m-d");?></small></div>
+        <textarea name="article" id="article" cols="30" rows="10" required>...</textarea>
         <input type="submit" value="Soumettre">
 </form>
