c97ea707da51c1cf5d47529f5a2122f008bc27b5
JanGaud <JanisGaudreault.jg@gmail.com>
2022-09-16 00:40:41 -0400

    Update connex.php

--- web21622/mvc/lib/connex.php
+++ web21622/mvc/lib/connex.php
@@ -1,6 +1,6 @@
 <?php
 
-// $con = mysqli_connect("localhost", "e2194798","7tbGLCR30GjRQwljrmti", "e2194798");
-$con = mysqli_connect("localhost", "root", "", "forum-college");
+$con = mysqli_connect("localhost", "e2194798", "7tbGLCR30GjRQwljrmti", "e2194798");
+// $con = mysqli_connect("localhost", "root", "", "forum-college");
 mysqli_set_charset($con,"utf8");
 ?>
